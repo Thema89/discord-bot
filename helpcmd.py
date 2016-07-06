2dc8dc62f5bcc0ee2631cf80fc74a9e0bf64d489
Angelos H <Angelos H>
2016-07-06 20:14:07 +0300

    Made it so only users who have permission to run commands can see it by typing plain "$help". If they don't but they do "$help cmd" where they can't run the command, in the output it will be the same as before + a little category that says "Perms Needed"

--- helpcmd.py
+++ helpcmd.py
@@ -3,13 +3,16 @@ import asyncio
 import gc
 
 class Command:
-    def __init__(self, cdesc, cname, args, adv, example):
+    def __init__(self, cdesc, cname, args, adv, example, perms):
         self.desc = cdesc
         self.name = cname
         self.args = args
         self.adv = adv
         self.example = example
-        #self.perms = perms
+        self.perms = perms
+
+    def set_perms(self, perms):
+        self.perms = perms
 
 cchannel = Command(
     cdesc='Creates a channel with the `name`.',
@@ -17,7 +20,7 @@ cchannel = Command(
     args='name;type',
     adv='`<name>` can be a single word in lowercase, any ammount of words inside "", or `current`. `current` will set the channel name to the game you are playing. `<type>` can be an intiger, `private`, `party` or `public`. If it is an intiger, that will be the client limit. If it is `private`, only whitelisted users will be able to join. To whitelist you need to do `$echannel <channel name>` and then use `#whitelist <name>`. If it is `party`, then you must specify party members in the syntax `name1;name2;name3`. It will basically automatically whitelist all the names when the channel is initialized. You can use the normal whitelist to add more members later. `public` makes it so even users without perms can join the channel (Keep in mind you could just whitelist them and they could join even without perms)',
     example='`$cchannel something 8` will return a channel named "Something" with user limit 8\n `$cchannel "Something Cool" private` will return a whitelist-only channel named "Something Cool" with unlimited user limit but whitelist only (giving a name inside "" will be the literal name of the channel)\n `$cchannel current party nondy;agouraki`, which will create a private channel with the creator, agouraki and nondy whitelisted by default.',
-    #perms=True
+    perms=True
 )
 dchannel = Command(
     cdesc='Deletes the channel with the **exact** name',
@@ -25,7 +28,7 @@ dchannel = Command(
     args='name',
     adv='You must be the owner of the channel to delete it.',
     example='`$dchannel Sicrit Club` (Using "" on $dchannel is optional)',
-    #perms=True
+    perms=True
 )
 echannel = Command(
     cdesc='Edits a channel.',
@@ -33,7 +36,7 @@ echannel = Command(
     args='name',
     adv='After calling `$echannel <channel>` the bot will wait for you to input an edit command.\n The valid edit commands are the following:\n `#name <name>` - Replaces the current channel name with the `<name>`\n `#limit <limit>` - Replaces the user limit with `<limit>`\n `#whitelist <user>` - Whitelists the `<user>` to your private channel\n `#lock` - Locks the channel, making it private, whitelisting all users already in the channel.\n `#unlock` - Unlocks the channel, deletes the whitelist, and makes it so users with perms can join.',
     example='`$echannel Sicrit Club` then after the bot responds: `#name Not Sicrit Club`',
-    #perms=True
+    perms=True
 )
 highnoon = Command(
     cdesc="It's High Noon somewhere in the world. This command tells you where.",
@@ -41,7 +44,8 @@ highnoon = Command(
     args='null',
     adv='null',
     example='null',
-    #perms=True
+    perms=True
+
 )
 purge = Command(
     cdesc="Purges a user from a channel. (Deletes their messages)",
@@ -49,7 +53,7 @@ purge = Command(
     args='name;limit',
     adv='Checks the last 100 messages in the channel by default. To make it look in more or less messages, just specify a `<limit>` (should be an intiger)',
     example='`$purge nondy 40`',
-    #perms=True
+    perms='manage_messages'
 )
 
 cmds = []
@@ -72,12 +76,31 @@ def get_cmd(cmnd):
         if cmd.name == cmnd:
             return cmd
 
+def get_perms(message, cmd):
+    if type(cmd.perms) != bool:
+        perms = "role.permissions." + cmd.perms
+        for role in message.author.roles:
+            if eval(perms):
+                return True
+                break
+    else:
+        return cmd.perms
+
+def perms_format(cmd):
+    perms = cmd.perms.split('_')
+    perms_str = ''
+    for perm in perms:
+        perms_str += '{} '.format(perm.title())
+
+    return perms_str
+
+
 async def print_all(message, client):
     msg = "**The following commands are available**:\n"
     for cmd in cmds:
-        args = get_args(cmd)
-
-        msg += '\n`${0} {1}` - {2}'.format(cmd.name, args, cmd.desc)
+        if get_perms(message, cmd) == True:
+            args = get_args(cmd)
+            msg += '\n`${0} {1}` - {2}'.format(cmd.name, args, cmd.desc)
 
     msg += '\n\n **You can use** `$help <command>` **for more information on each command** (Example: `$help echannel`)'
 
@@ -96,6 +119,11 @@ async def print_spec(message, client, cmd):
 
     if cmd.example != 'null':
         msg += "\n__Example__: {}".format(cmd.example)
+
+    if type(cmd.perms) != bool:
+        perms = perms_format(cmd)
+        msg += "\n__Perms Needed__: {}".format(perms)
+
 
     await client.send_message(message.channel, msg)
 
